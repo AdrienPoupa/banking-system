f089029a1d7a86032b00c2a905cf25faa7a6d912
AdrienPoupa <adrien@poupa.fr>
2016-05-17 16:43:15 +0800

    Threads

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include "src/Address.h"
-#include "src/User.h"
 #include "src/Bank.h"
-#include "src/Client.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-
-    Address* address = new Address(1, "Test", "67000", "Test", "Malaysia");
-    cout << *address << endl;
-
-
     Bank* bank = Bank::getSingleton();
     bank->run();
 
