ab1f33c331280427c3920190c1cf79d94fa8b035
adrienleroy <adrien.le.roy@efrei.net>
2016-05-19 10:20:40 +0800

    - Contact Advisor

--- main.cpp
+++ main.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 #include "src/Bank.h"
-#include <iostream>
-#include <iomanip>
-#include <ctime>
+#include "src/Date.h"
 
 using namespace std;
 
 int main() {
    /* Bank* bank = Bank::getSingleton();
     bank->run();*/
-    time_t rawtime;
-    struct tm * timeinfo;
-    char buffer[80];
-
-    time (&rawtime);
-    timeinfo = localtime(&rawtime);
-
-    strftime(buffer,80,"%d-%m-%Y %I:%M:%S",timeinfo);
-    std::string str(buffer);
-
-    std::cout << str;
-
+Date* d = new Date();
+cout << *d;
     return 0;
 }
